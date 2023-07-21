e15c22a62dafb53a5c61d2dfcaad62256177e1e4
diogo <diogo.marcondes2004@gmail.com>
2023-07-20 21:13:40 -0300

    Config image error

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ const text = document.querySelector('.text-meal')
 const button = document.querySelectorAll('.btn-meals')
 const img = document.querySelector('.img-meal')
 const imgs = [
-    '/img/Italian-breakfast.jpg',
-    '/img/Italian-pasta.jpg',
-    '/img/Italian-pizza.jpg'
+    '/img/italianbreakfast.jpg',
+    '/img/italianpasta.jpg',
+    '/img/italianpizza.jpg'
 ] 
 const littleText = [
     '<h1>Breakfast</h1><p>Curabitur malesuada justo a neque dapibus, ut sodales urna volutpat, isus est mattis enim, nec eleifend nisi neque nec ex. Cras non consectetur nibh, non varius sem.</p>',
